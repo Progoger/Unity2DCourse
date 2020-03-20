7f26ed54c57f50adcb17a449ffd2bda25d02afc2
Progoger <rastorguev10@gmail.com>
2020-03-20 20:20:13 +0300

    Lesson9

---
+++ SecondSection/NumberWizard/NumberWizardConsole/Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to number wizard, yo man");
+        Debug.Log("Bro, pick a number, keep it in secret...");
+        Debug.Log("The highest number for ma boy is: 1000");
+        Debug.Log("The lowest number for ma boy is: 0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
